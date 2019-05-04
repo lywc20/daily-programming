8cbcb32774ee24c5aaad3387a170c8f3412290ac
Vincent <lywc420@gmail.com>
2019-05-04 16:01:07 -0700

    Add inOrderTraversal to IterativeTreeTraversal.py

--- Python/IterativeTreeTraversal.py
+++ Python/IterativeTreeTraversal.py
@@ -21,7 +21,7 @@ def insert(root,val):
                 tmp.left = node
                 break
 
-def Iterative_Prefix(root):
+def iterativePreOrder(root):
     if not root:
         return []
     result = []
@@ -38,8 +38,25 @@ def Iterative_Prefix(root):
 
     print(result)
 
-root = Node(4)
-for i in [2,3,6,7]:
+def iterativePostOrder(root):
+    if not root:
+        return []
+    result = []
+    stack = []
+    stack.append(root)
+    while stack:
+        tmp = stack[-1]
+        if tmp.left and tmp.left.val not in result:
+            stack.append(tmp.left)
+        else:
+            stack.pop()
+            result.append(tmp.val)
+            if tmp.right:
+                stack.append(tmp.right)
+    print(result)
+
+root = Node(5)
+for i in [3,1,0,2,8,6,10,11]:
     insert(root,i)
 
-Iterative_Prefix(root)
+iterativePostOrder(root)
