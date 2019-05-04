65883b46372e12797dc5c747cbbee627ed7d02aa
Vincent <lywc420@gmail.com>
2019-05-04 16:30:34 -0700

    Modify postOrderTraversal in IterativeTreeTraversal

--- Python/IterativeTreeTraversal.py
+++ Python/IterativeTreeTraversal.py
@@ -39,22 +39,18 @@ def iterativePreOrder(root):
     print(result)
 
 def iterativePostOrder(root):
-    if not root:
-        return []
     result = []
     stack = []
-    stack.append(root)
-    while stack:
-        tmp = stack[-1]
-        if tmp.left and tmp.left.val not in result:
-            stack.append(tmp.left)
-        else:
-            stack.pop()
-            result.append(tmp.val)
-            if tmp.right:
-                stack.append(tmp.right)
-    print(result)
-
+    tmp = root
+    while stack or tmp:
+        while tmp:
+            stack.append(tmp)
+            tmp = tmp.left
+        tmp = stack.pop()
+        result.append(tmp.val)
+        tmp = tmp.right
+    for e in result:
+        print(e)
 root = Node(5)
 for i in [3,1,0,2,8,6,10,11]:
     insert(root,i)
