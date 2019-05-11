822556c737c133e8dbf3f3fd5df82673ade247da
Vincent <lywc420@gmail.com>
2019-05-11 15:30:40 -0700

    Add three methods for performing level order traversal in IterativeTreeTraversal

--- Python/IterativeTreeTraversal.py
+++ Python/IterativeTreeTraversal.py
@@ -71,8 +71,79 @@ def iterativeInOrder(root):
             tmp = tmp.right
     for e in result:
         print(e.val)
-root = Node(5)
-for i in [4,-6,30,-16,2,48,3]:
+
+## Two queue method
+def levelOrderTraversal1(root):
+    res = []
+    q0 = []
+    q1 = []
+    q0.append(root)
+    while q0 or q1:
+        curr = []
+        while q0:
+            peek = q0.pop(0)
+            curr.append(peek.val)
+            if peek.left:
+                q1.append(peek.left)
+            if peek.right:
+                q1.append(peek.right)
+        res.append(curr)
+        q0 = q1
+        q1 = []
+    for e in res:
+        print(e)
+
+## Single queue method with dummy variable
+def levelOrderTraversal2(root):
+    res = []
+    queue = []
+    queue.append(root)
+    queue.append(None)
+    curr = []
+    while queue:
+       tmp = queue.pop(0)
+       if tmp == None:
+           res.append(curr)
+           curr = []
+           if len(queue) > 1:
+               queue.append(None)
+       else:
+           curr.append(tmp.val)
+           if tmp.left:
+               queue.append(tmp.left)
+           if tmp.right:
+               queue.append(tmp.right)
+    for e in res:
+        print(e)
+
+## Maintaining count of each level
+def levelOrderTraversal3(root):
+    res = []
+    queue = []
+    curr = []
+    currCount = 1
+    newCount = 0
+    queue.append(root)
+    while queue:
+        peek = queue.pop(0)
+        currCount -= 1
+        curr.append(peek.val)
+        if peek.left:
+            queue.append(peek.left)
+            newCount += 1
+        if peek.right:
+            queue.append(peek.right)
+            newCount += 1
+        if currCount == 0:
+            currCount = newCount
+            newCount = 0
+            res.append(curr)
+            curr = []
+    for e in res:
+        print(e)
+
+root = Node(2)
+for i in [-8,20,-16,-4,10,25,-7,15]:
     insert(root,i)
 
-iterativePostOrder(root)
+levelOrderTraversal3(root)
